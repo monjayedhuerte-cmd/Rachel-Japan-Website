1d21f78b289dfab86b3279d19f6ae27a02499f2c
monjayedhuerte-cmd <monjayed.huerte@gmail.com>
2026-08-03 10:47:44 +0800

    Update Website

---
+++ js/loader.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", ()=>{
+
+    const loader=document.getElementById("loader");
+
+    setTimeout(()=>{
+
+        loader.style.opacity="0";
+
+        setTimeout(()=>{
+
+            loader.style.display="none";
+
+        },1500);
+
+    },3500);
+
+});
